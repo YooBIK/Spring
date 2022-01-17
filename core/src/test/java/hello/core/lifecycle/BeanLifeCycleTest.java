563886d5e05637bc8ee061f1cc8699ea17cfd96c
YBI <quddlr96@naver.com>
2022-01-17 17:49:52 +0900

    ~0117

---
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,30 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.Annotation;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(lifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class lifeCycleConfig{
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
