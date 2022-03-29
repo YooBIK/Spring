80156dd4186e9d96257e921cfef965c2627dc849
YBI <quddlr96@naver.com>
2022-03-29 18:45:15 +0900

    Servlet + JSP + MVC 패턴 사용 ~0329

--- SpringMVC/servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ SpringMVC/servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -18,7 +18,7 @@ public class MemberRepository {
     }
 
     public Member save(Member member){
-        member.setId(sequence++);
+        member.setId(++sequence);
         store.put(member.getId(),member);
         return member;
     }
