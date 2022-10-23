bc73027ca0026674471aecc222e88137b62e82a4
YooByeongIk <quddlr96@naver.com>
2022-10-23 15:27:36 +0900

    [ADD] HTTP Request - JSON

--- SpringMVC/springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ SpringMVC/springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -38,7 +38,15 @@ public class RequestBodyStringController {
         responseWriter.write("ok");
     }
 
-    //
+    /**
+     * HttpEntity: HTTP header, body 정보를 편리하게 조회
+     * - 메시지 바디 정보를 직접 조회(@RequestParam X, @ModelAttribute X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     *
+     * 응답에도 HttpEntity 사용 가능
+     * - 메시지 바디 정보 직접 반환(view 조회X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     */
     @PostMapping("/request-body-string-v3")
     public void requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
         String messageBody = httpEntity.getBody();
@@ -46,6 +54,15 @@ public class RequestBodyStringController {
         //responseWriter.write("ok");
     }
 
+    /**
+     * @RequestBody
+     * - 메시지 바디 정보를 직접 조회(@RequestParam X, @ModelAttribute X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     *
+     * @ResponseBody
+     * - 메시지 바디 정보 직접 반환(view 조회X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     */
     @ResponseBody
     @PostMapping("/request-body-string-v4")
     public String requestBodyStringV4(@RequestBody String messageBody) throws IOException {
