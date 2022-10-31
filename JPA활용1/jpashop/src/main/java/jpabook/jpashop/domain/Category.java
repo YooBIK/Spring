01cfa8fbad545aa8f38cb5709bf3ca14f915d7ff
YooByeongIk <quddlr96@naver.com>
2022-11-01 02:10:32 +0900

    [ADD] JPA 활용 Entity Class 설계 및 생성

---
+++ JPA활용1/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+
+    /*
+        #연관 관계 편의 메소드
+            양방향 연관 관계에서 한번의 setting으로 양방향 모두 설정
+     */
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
+
+}
+
+
+
