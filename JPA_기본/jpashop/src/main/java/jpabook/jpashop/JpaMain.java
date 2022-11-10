76d2d444386d17c7f76b7ab31eca197b33a351dc
YooByeongIk <quddlr96@naver.com>
2022-11-10 23:40:19 +0900

    [ADD] 실전 예제1 요구사항 분석 및 기본 매핑

---
+++ JPA_기본/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        try{
+            transaction.commit();
+        }catch (Exception e){
+            transaction.rollback();
+        }finally {
+            entityManager.close();
+        }
+        entityManagerFactory.close();
+    }
+}
