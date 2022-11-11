91a6eb0a5d67926ed6d64442674cd1e675f84a37
YooByeongIk <quddlr96@naver.com>
2022-11-11 15:29:29 +0900

    [ADD] 연관관계 매핑 - 양방향 연관관계

--- JPA_기본/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ JPA_기본/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -25,11 +26,21 @@ public class JpaMain {
             entityManager.persist(member);
 
             /*
+            영속성 컨텍스트의 쿼리들을 DB에 전송 + 영속성 컨택스트 초기화
+             */
+            entityManager.flush();
+            entityManager.clear();
+
+            /*
             연관관계 매핑을 사용해 컬렉션에서 객체를 꺼내는 것처럼 사용 가능
+            mapped by 를 활용해 서로 참조 가능!!
              */
             Member findMember = entityManager.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam.getId() = " + findTeam.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for(var v : members){
+                System.out.println("member = " + v.getName());
+            }
 
             transaction.commit();
         }catch (Exception e){
--- JPA_기본/jpashop/src/main/java/jpabook/jpashop/domain/Team.java
+++ JPA_기본/jpashop/src/main/java/jpabook/jpashop/domain/Team.java
@@ -1,9 +1,8 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -12,6 +11,10 @@ public class Team {
     @GeneratedValue
     @Column(name = "TEAM_ID")
     private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -29,5 +32,11 @@ public class Team {
         this.name = name;
     }
 
-    private String name;
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
 }
