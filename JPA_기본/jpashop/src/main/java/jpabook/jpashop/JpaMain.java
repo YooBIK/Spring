e6affa80190269cbcdfb32449ae5e006ef4070ff
YooByeongIk <quddlr96@naver.com>
2022-11-22 22:11:29 +0900

    [UPDATE] Setting Update

--- JPA_기본/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ JPA_기본/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,13 +1,11 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Team;
+import jpabook.jpashop.domain.Book;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,38 +15,10 @@ public class JpaMain {
         transaction.begin();
         try{
 
-            /*
-            2. 연관 관계 주인인 Member에서 Team을 추가해야함!!
-             */
-            Team team = new Team();
-            team.setName("TeamA");
-            entityManager.persist(team);
-
-            Member member = new Member();
-            member.setName("helloA");
-            member.setTeam(team);
-            entityManager.persist(member);
-
-            /*
-             3. 하지만, 영속성 컨텍스트에 담겨있을때는, LIST가 비어있음!! (순수한 객체 상태이다.)
-                !! 웬만하면 양방향 연관 관계에서 양쪽 다 값을 세팅하자 !!
-                !! 이 때, 연관관계 편의 메소드를 작성하자. !!
-                여기선 Member의 setTeam에 로직을 추가했다. setter를 사용하기 보단 새로 정의하자 !!
-             */
-            //team.getMembers().add(member);
-
-//            /*
-//              **연관관계의 주인에 값을 입력해야함**
-//              ** 1. Team은 연관관계의 주인이 아니기 때문에, 읽기 전용이다. 즉 DB에 입력한 사항이 반영되지 않음!!
-//             */
-//            Team team = new Team();
-//            team.setName("TeamA");
-//            team.getMembers().add(member);
-//            entityManager.persist(team);
-
-
-            entityManager.flush();
-            entityManager.clear();
+            Book book = new Book();
+            book.setName("책A");
+            book.setAuthor("유병익");
+            entityManager.persist(book);
 
             transaction.commit();
         }catch (Exception e){
