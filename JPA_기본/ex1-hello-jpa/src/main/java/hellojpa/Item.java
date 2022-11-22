4e5d8c13fe4c97edc9026fd57b558da925afcb29
YooByeongIk <quddlr96@naver.com>
2022-11-22 19:50:47 +0900

    [ADD] 상속 관계 매핑 - TABLE PER CLASS 전략

--- JPA_기본/ex1-hello-jpa/src/main/java/hellojpa/Item.java
+++ JPA_기본/ex1-hello-jpa/src/main/java/hellojpa/Item.java
@@ -3,9 +3,15 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+/*
+* 구현 클래스마다 테이블을 생성하는 전략은 추천 X
+*  WHY? 여러 테이블을 함께 조회할 때, 성능이 안좋음
+*       자식 테이블들 통합 쿼리하기가 어려움
+ */
+
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 //@DiscriminatorColumn //SINGLE_TABLE 전략은 해당 애노테이션이 없어도 DTYPE 컬럼이 생성된다.
-public class Item {
+public abstract class Item {
 
     @Id @GeneratedValue
     private Long id;
